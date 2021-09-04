b23fce6e09502bae64da5bb7634192eadaec6bdb
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-09-04 23:10:55 +0530

    fetch a post

---
+++ client/src/component/editPost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class EditPost extends Component {
+    state = {  }
+    render() { 
+        return (
+            <>
+            </>
+          );
+    }
+}
+ 
+export default EditPost;
+<>
+</>
